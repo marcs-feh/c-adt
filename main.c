6514c75c8b27828f4731e823995ed56a4956dd83
marcs-feh <marcosfehlauer@protonmail.com>
2022-09-13 13:09:26 -0300

    resize and append

--- main.c
+++ main.c
@@ -9,10 +9,37 @@
 
 void vec_test(){
 	Vec v = Vec_new();
+
 	TEST_LOG("Vec_new");
 	TEST_EQ(0, v.len);
 	TEST_EQ(VEC_INIT_CAP, v.cap);
 	TEST_EQ(false, (v.data == NULL));
+
+	TEST_LOG("Vec_append");
+	Vec_append(&v, 0);
+	Vec_append(&v, 1.5);
+	Vec_append(&v, -3.2);
+	// Expect [0, 1.5, -3.2]
+	TEST_EQ(0, v.data[0]);
+	TEST_EQ(1.5f, v.data[1]);
+	TEST_EQ(-3.2f, v.data[2]);
+	TEST_EQ(3, v.len);
+
+	TEST_LOG("Appending 40 elements.");
+	for(size_t i = 0; i < 40; i++)
+		Vec_append(&v, 0);
+	TEST_EQ(43, v.len);
+
+	TEST_LOG("Resize and append");
+	Vec_resize(&v, 3);
+	Vec_append(&v, 0);
+	// Expect [0, 1.5, -3.2]
+	TEST_EQ(0, v.data[0]);
+	TEST_EQ(1.5f, v.data[1]);
+	TEST_EQ(-3.2f, v.data[2]);
+	TEST_EQ(4, v.len);
+	TEST_EQ(7, v.cap);
+
 	TEST_LOG("Vec_del");
 	Vec_del(&v);
 	TEST_EQ(0, v.len);
--- vec.c
+++ vec.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #define VEC_INIT_CAP 16
+#define MIN(a, b) (((a <= b) * a) + ((b < a) * b))
 
 typedef struct {
 	size_t cap;
@@ -27,6 +28,29 @@ void Vec_del(Vec *v){
 	v->cap = 0;
 	free(v->data);
 	v->data = NULL;
+}
+
+void Vec_resize(Vec *v, size_t n){
+	if(v == NULL) return;
+	float *newdata = realloc(v->data, n * sizeof(*newdata));
+
+	// Failed alloc.
+	if(newdata == NULL) return;
+
+	v->data = newdata;
+	v->cap = n;
+	if(v->len > n)
+		v->len = n;
+}
+
+void Vec_append(Vec *v, float e){
+	if(v == NULL) return;
+	if(v->len + 1 >= v->cap){
+		Vec_resize(v, (v->cap * 2) + 1);
+	}
+
+	v->data[v->len] = e;
+	v->len++;
 }
 
 
