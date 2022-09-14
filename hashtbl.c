6ec59078f727ccd0f30b1e5713f60f30a942e404
marcs-feh <marcosfehlauer@protonmail.com>
2022-09-14 12:46:25 -0300

    table/bucket init and del

--- hashtbl.c
+++ hashtbl.c
@@ -1,4 +1,8 @@
 #include <stddef.h>
+#include <stdlib.h>
+
+#define HASH_TABLE_MIN_BUCKETS 8
+#define HASH_TABLE_BUCKET_INIT_SIZE 16;
 
 typedef struct {
 	const char* key;
@@ -13,6 +17,62 @@ typedef struct {
 
 typedef struct {
 	TableBucket *buckets;
-	const size_t size;
+	size_t size;
 } HashTable;
 
+TableBucket Bucket_new(){
+	TableBucket b;
+	b.cap = HASH_TABLE_BUCKET_INIT_SIZE;
+	b.len = 0;
+	b.data = malloc(sizeof(*(b.data)) * b.cap);
+
+	// Failed alloc.
+	if(b.data == NULL){
+		b.cap = 0;
+		return b;
+	}
+
+	return b;
+}
+
+void Bucket_del(TableBucket *b){
+	if(b == NULL) return;
+	b->cap = 0;
+	b->len = 0;
+	free(b->data);
+	b->data = NULL;
+}
+
+HashTable Table_new(size_t buckets){
+	if(buckets < HASH_TABLE_MIN_BUCKETS)
+		buckets = HASH_TABLE_MIN_BUCKETS;
+
+	HashTable ht;
+	ht.size = buckets;
+	ht.buckets = malloc(sizeof(TableBucket) * ht.size);
+
+	// Failed alloc.
+	if(ht.buckets == NULL){
+		ht.size = 0;
+		return ht;
+	}
+
+	// Init buckets
+	for(size_t i = 0; i < ht.size; i++){
+		ht.buckets[i] = Bucket_new();
+	}
+
+	return ht;
+}
+
+void Table_del(HashTable *ht){
+	if(ht == NULL) return;
+	ht->size = 0;
+	// Free bucket data.
+	for(size_t i = 0; i < ht->size; i++){
+		Bucket_del(&(ht->buckets[i]));
+	}
+	// Free buckets.
+	free(ht->buckets);
+	ht->buckets = NULL;
+}
