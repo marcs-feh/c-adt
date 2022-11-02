b41ce98ce57d989efa34048dfd8eff34e80d17be
marcs-feh <marcosfehlauer@protonmail.com>
2022-11-02 14:06:04 -0300

    add panic

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "autotest.h"
 #include "tests.c"
+#include "panic.h"
 
 int main(int argc, const char** argv){
 	return 0;
--- panic.h
+++ panic.h
@@ -2,12 +2,16 @@
 #define INCLUDE_PANIC_H_
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 #define Panic(msg) {\
-	time_t sys_time_ = time(NULL); \
-	struct tm date_ = *localtime(&t); \
-	fprintf(""); \
+	time_t panic_timer_ = time(NULL);\
+	struct tm panic_date_ = *localtime(&panic_timer_);\
+	fprintf(stderr, "[%d-%02d-%02d %02d:%02d:%02d] PANIC: %s\n",\
+	       panic_date_.tm_year + 1900, panic_date_.tm_mon + 1, panic_date_.tm_mday,\
+	       panic_date_.tm_hour, panic_date_.tm_min, panic_date_.tm_sec, msg);\
+	exit(-1);\
 }
 
 #endif
