cd04eea87d855cac93efb008b03cd6d4f1c10126
marcs-feh <marcosfehlauer@protonmail.com>
2022-11-16 11:30:28 -0300

    add types

--- pair.h
+++
@@ -1,6 +0,0 @@
-#ifndef INCLUDE_PAIR_H_
-#define INCLUDE_PAIR_H_
-
-#define Pair(T1, T2) struct {T1 a; T2 b;}
-
-#endif
---
+++ types.h
@@ -0,0 +1,27 @@
+#ifndef INCLUDE_COMMON_TYPEDEFS_H_
+#define INCLUDE_COMMON_TYPEDEFS_H_
+
+#include <stdalign.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+typedef int8_t   i8;
+typedef int16_t  i16;
+typedef int32_t  i32;
+typedef int64_t  i64;
+
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef float    f32;
+typedef double   f64;
+
+typedef size_t        usize;
+typedef uintptr_t     uintptr;
+typedef unsigned char byte;
+/* typedef uint8_t byte; */
+
+#endif
